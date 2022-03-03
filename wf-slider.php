7ed5f80521c7f038f57a9bb510da23c326e22d97
Wallison Ferreira <contato.sanfer@gmail.com>
2022-03-03 15:08:05 -0500

    first commit

---
+++ wf-slider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Plugin Name: WF Slider
+ * Plugin URI: https://www.wordpress.org/wf-slider
+ * Description: Slider for pages and posts
+ * Version: 1.0
+ * Requires at least: 5.9
+ * Author: Wallison Ferreira
+ * Author URI: https://wallisonferreira.github.io
+ * License: GLP v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wf-slider
+ * Domain Path: /languages
+ */
+
+ /*
+WF Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WF Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with WF Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+if( ! defined('ABSPATH') ) {
+    exit;
+}

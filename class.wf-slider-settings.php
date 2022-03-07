d8a9491b1f06f4534e0369d054162cc6ad447525
Wallison Ferreira <contato.sanfer@gmail.com>
2022-03-07 15:37:41 -0500

    feat(settings): adds options settings

--- class.wf-slider-settings.php
+++ class.wf-slider-settings.php
@@ -9,8 +9,9 @@ if ( !class_exists( 'SliderSettings' ) ) {
             add_action( 'admin_init', array( $this, 'admin_init' ) );
         }
 
+        # Gerencia o slider
         public function admin_init() {
-            
+
             register_setting( 'wf_slider_group', 'wf_slider_options' );
 
             add_settings_section(
@@ -20,6 +21,14 @@ if ( !class_exists( 'SliderSettings' ) ) {
                 'wf_slider_page1' // declara id da página
             );
 
+            add_settings_section(
+                'wf_slider_second_section', // declara id da seção
+                'Other Plugin Options',
+                null,
+                'wf_slider_page2' // declara id da página
+            );
+
+            # Observação do shortcode do slider
             add_settings_field( 
                 'wf_slider_shortcode', 
                 'Shortcode', 
@@ -27,11 +36,80 @@ if ( !class_exists( 'SliderSettings' ) ) {
                 'wf_slider_page1', // id da página que o campo deve aparecer
                 'wf_slider_main_section', // id da seção que o campo deve aparecer
             );
+
+            # Campo do título do slider
+            add_settings_field( 
+                'wf_slider_title', 
+                'Slider Title', 
+                array( $this, 'wf_slider_title_callback' ), 
+                'wf_slider_page2', // id da página que o campo deve aparecer
+                'wf_slider_second_section', // id da seção que o campo deve aparecer
+            );
+
+            # Mostrar bullets do slider
+            add_settings_field( 
+                'wf_slider_bullets', 
+                'Display Bullets', 
+                array( $this, 'wf_slider_bullets_callback' ), 
+                'wf_slider_page2', // id da página que o campo deve aparecer
+                'wf_slider_second_section', // id da seção que o campo deve aparecer
+            );
+
+            # Escolher estilo do slider
+            add_settings_field( 
+                'wf_slider_style', 
+                'Slider Style', 
+                array( $this, 'wf_slider_style_callback' ), 
+                'wf_slider_page2', // id da página que o campo deve aparecer
+                'wf_slider_second_section', // id da seção que o campo deve aparecer
+            );
         }
 
         public function wf_slider_shortcode_callback() {
             ?>
             <span>Use the shortcode [wf_slider] to display the slider in any page/post/widget</span>
+            <?php
+        }
+
+        public function wf_slider_title_callback() {
+            ?>
+                <input 
+                type="text"
+                name="wf_slider_options[wf_slider_title]"
+                id="wf_slider_title"
+                value="<?php echo isset( self::$options['wf_slider_title'] ) ? esc_attr( self::$options['wf_slider_title'] ) : ''; ?>">
+            <?php
+        }
+
+        public function wf_slider_bullets_callback() {
+            ?>
+                <input 
+                    type="checkbox"
+                    name="wf_slider_options[wf_slider_bullets]"
+                    id="wf_slider_bullets"
+                    value="1"
+                    <?php
+                        if ( isset( self::$options['wf_slider_bullets'] ) ) {
+                            checked( "1", self::$options['wf_slider_bullets'], true );
+                        }
+                    ?>
+                />
+                <label for="mv_slider_bullets">Whether to display bullets or not</label>
+            <?php
+        }
+
+        public function wf_slider_style_callback() {
+            ?>
+                <select 
+                    name="wf_slider_options[wf_slider_style]" 
+                    id="wf_slider_style">
+                    <option value="style-1"
+                        <?php isset( self::$options['wf_slider_style'] ) ? selected( "style-1", self::$options['wf_slider_style'], true 
+                        ) : '';?>>Style-1</option>
+                    <option value="style-2"
+                        <?php isset( self::$options['wf_slider_style'] ) ? selected( "style-2", self::$options['wf_slider_style'], true 
+                        ) : '';?>>Style-2</option>
+                </select>
             <?php
         }
     }
--- views/settings-page.php
+++ views/settings-page.php
@@ -4,6 +4,7 @@
     <?php 
         settings_fields( 'wf_slider_group' ); // cria hiddens fields e nonces de verificação
         do_settings_sections( 'wf_slider_page1' ); // referencia a page na qual será mostrada no formulário
+        do_settings_sections( 'wf_slider_page2' ); // referencia a page na qual será mostrada no formulário
         submit_button( 'Save Settings' );
     ?>
     </form>
